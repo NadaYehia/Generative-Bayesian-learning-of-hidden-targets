6bd3961e114587fd79646954ea2124bc85fd7d42
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-01-31 14:07:26 -0500

    rename the sampler scripts
    renamed the sampler scripts inside to more intuitive names

--- Sampling_next_actions.m
+++ Sampling_next_actions.m
@@ -4,9 +4,9 @@ function [mu_anchors,omega_anchors,anchors_no]= Sampling_next_actions(posterior,
 
 if(strcmp(sampler,'proportional'))
 
-    [mu_anchors,omega_anchors,anchors_no]=anchors_sampler_nn_merge (posterior,initial_ancs,As,Os,dist_criterion,r_bounds,c_bounds);
+    [mu_anchors,omega_anchors,anchors_no]=anchors_prop_sampler_wnn_merge (posterior,initial_ancs,As,Os,dist_criterion,r_bounds,c_bounds);
 else
-    [mu_anchors,omega_anchors,anchors_no(k)]=anchors_sampler_MuliPeakPost (posterior,initial_ancs,As,Os,dist_criterion,r_bounds,c_bounds);
+    [mu_anchors,omega_anchors,anchors_no(k)]=anchors_peak_sampler(posterior,initial_ancs,As,Os,dist_criterion,r_bounds,c_bounds);
  
 end
 
