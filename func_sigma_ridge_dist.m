7bd7d1d7e6f7414f3506610ebfe32a8f6a19435f
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-01-31 13:28:56 -0500

    fixing the distance measure
    corrected the distance measure to be k*sigma in one direction (that's the speed) but not both. the  euclidean distance equivalent to 1 sigma away is either sigma ridge per direction or 1/sqrt(2) sigma in both directions, not a full sigma in each.

--- func_sigma_ridge_dist.m
+++ func_sigma_ridge_dist.m
@@ -1,17 +1,12 @@
-function dist=func_sigma_ridge_dist(k,sigma_ridge)
+function dist=func_sigma_ridge_dist(k,sigma_ridge,As,Os)
 
-% first find the equivalent distance to one sigma in both x&y around a
-% point in the action space
- As=[100:2:800];
- Os=[-pi/2:2*pi/1000:pi/2];
-
- x0= 200;
-y0=-0.5;
 
 increment_x=2*k*sigma_ridge;
 increment_y= (2*pi/1000)*k*sigma_ridge;
 
-D1=[0 0; increment_x increment_y; increment_x increment_y; 0 0];
+% D1=[0 0;  0 increment_y;  0 increment_y; 0 0];
+D1=[0 0; increment_x 0; increment_x 0; 0 0];
+
 
 D_= (D1)*[1/(std(As)^2) 0; 0  1/(std(Os)^2)]*(D1');
 
