d2014899c80492ea395dca1bd8555d08d640efbc
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-02-05 12:00:50 -0500

    fix the distance calc in propor sampler

--- sampler_toolbox/anchors_prop_sampler_wnn_merge.m
+++ sampler_toolbox/anchors_prop_sampler_wnn_merge.m
@@ -1,4 +1,4 @@
-function [mu_anchors,omega_anchors,n]=anchors_prop_sampler_wnn_merge (posterior,n,As,Os,dist_criterion,r_bounds,c_bounds)
+function [mu_anchors,omega_anchors,n]=anchors_prop_sampler_wnn_merge (posterior,n,As,Os,dist_criterion)
 
     action_array=[1:size(posterior,1)*size(posterior,2)];
     l_ind= datasample(action_array,n,'Replace',false,'Weights',(posterior(:)));
@@ -7,9 +7,6 @@ function [mu_anchors,omega_anchors,n]=anchors_prop_sampler_wnn_merge (posterior,
     
     mu_anchors=As(mu_temp_ind);
 
-    if (max(omega_temp_ind)>size(Os,2))
-        debugg=1;
-    end
     omega_anchors=Os(omega_temp_ind);
     
     % xx=(nxn)x1 column vector of the first dimension in anchors: mu
@@ -25,8 +22,9 @@ function [mu_anchors,omega_anchors,n]=anchors_prop_sampler_wnn_merge (posterior,
     yy2= repmat(omega_anchors',n,1);
 
     D1=[xx xx2]-[yy yy2];
-
-    D_= (D1)*[1/(std(As)^2) 0; 0  1/(std(Os)^2)]*(D1');
+    rng_As=As(end)-As(1);
+    rng_Os=Os(end)-Os(1); 
+    D_= (D1)*[1/((rng_As)^2) 0; 0  1/((rng_Os)^2)]*(D1');
 
     D=sqrt(reshape(diag(D_),[n n]));
     D(D==0)=nan;
@@ -70,15 +68,12 @@ function [mu_anchors,omega_anchors,n]=anchors_prop_sampler_wnn_merge (posterior,
         xx2=repmat(omega_anchors,n,1);
         xx2=xx2(:);
     
-        % yy= (nn)x1 column vector of the first dimension in anchors: mu
-        % repeat mu_anchors' vector 10 times and omega_anchors' vector also
-        % 10x
         yy=repmat(mu_anchors',n,1);
         yy2= repmat(omega_anchors',n,1);
     
         D1=[xx xx2]-[yy yy2];
     
-        D_= (D1)*[1/(std(As)^2) 0; 0  1/(std(Os)^2)]*(D1');
+        D_= (D1)*[1/((rng_As)^2) 0; 0  1/((rng_Os)^2)]*(D1');
     
         D=sqrt(reshape(diag(D_),[n n]));
         D(D==0)=nan;
