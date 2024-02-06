725491db04a61ac2f4c4b6b45f774373c0d601cb
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-02-06 11:57:29 -0500

    Update main.m

--- main.m
+++ main.m
@@ -33,7 +33,7 @@ Os=linspace(min_angle,max_angle,n);
 clearnce=0.5; %loop width in radians
 sampler='proportional'; 
 % sampler='peak_sampler';
-draw_flg=1;
+draw_flg=0;
 target_num=2;
 k=1;
 merging_criterion= sigma_ridge/n; % converting sigma ridge from pixels distance to normalized dist.
@@ -41,7 +41,7 @@ win=1;
 tic
 
 % start agents trials
-for agent=1:ags
+parfor agent=1:ags
 
 initial_ancs=10;
 dd=sum(env.blocks(target_num));
