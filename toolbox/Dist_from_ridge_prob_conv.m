2a79730580872f3e15b311115a91b1fd567caa23
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-02-07 13:47:44 -0500

    rmv unnecessary boundary conditions

--- toolbox/Dist_from_ridge_prob_conv.m
+++ toolbox/Dist_from_ridge_prob_conv.m
@@ -1,25 +1,6 @@
 function L1=Dist_from_ridge_prob_conv(x_,y_,sigma,As,Omegas)
 
 
-% eliminate speeds outside our posterior action space.
-[bx1]=find(x_<As(1));
-[bx2]=find(x_>As(end));
-
-bx=[bx1, bx2];
-
-x_(bx)=[];
-y_(bx)=[];
-
-% eliminate angles outside our posterior actions space.
-[by1]=find(y_<Omegas(1));
-[by2]=find(y_>Omegas(end));
-
-by=[by1,by2];
-
-x_(by)=[];
-y_(by)=[];
-
-%
 L=zeros(size(Omegas,2),size(As,2));
 
 for s=1:numel(y_)
