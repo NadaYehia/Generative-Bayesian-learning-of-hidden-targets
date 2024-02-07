049d8f083390b79fcb5e6a49d561bfaa9192bae6
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-02-07 12:38:07 -0500

    fix the compensatory noise for this generative model

--- generative_model_toolbox/connect_actions_with_smooth_trajectory.m
+++ generative_model_toolbox/connect_actions_with_smooth_trajectory.m
@@ -4,7 +4,8 @@ T=1000;
 k=(T)/pi;
 arena=env.arena_dimensions;
 a=2;
-speed_noise=gamrnd(a*(1.6*sigma_ridge*speed_step),1/a);
+c=0.25; %1.6
+speed_noise=gamrnd(a*(c*sigma_ridge*speed_step),1/a);
 % speed_noise=0;
 
 x_op=[];
