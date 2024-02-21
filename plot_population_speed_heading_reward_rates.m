e51da51186bcef4ccfb7cdfc7dc0e72a17ddb670
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-02-21 13:23:28 -0500

    Update plot_population_speed_heading_reward_rates.m

--- plot_population_speed_heading_reward_rates.m
+++ plot_population_speed_heading_reward_rates.m
@@ -3,7 +3,7 @@ trials=size(corr_pop_avg,2);
 
 figure;
 subplot(1,4,3), shadedErrorBar(1:trials, mean(corr_pop_avg(:,1:trials)) ,std( corr_pop_avg(:,1:trials))./sqrt(size(corr_pop_avg,1) )./scale,{'color','g'}); hold on;
-
+ylim([0 1]);
 hold on, subplot(1,4,1),shadedErrorBar(1:trials, mean(mu_pop_avg(:,1:trials)) ,std( mu_pop_avg(:,1:trials))./sqrt(size(mu_pop_avg,1) )./scale,{'color','g'}); hold on;
 
 hold on, subplot(1,4,4), shadedErrorBar(1:trials, mean(anchors_no_pop(:,1:trials)) ,std( anchors_no_pop(:,1:trials))./sqrt(size(anchors_no_pop,1) )./scale,{'color','g'}); hold on;
