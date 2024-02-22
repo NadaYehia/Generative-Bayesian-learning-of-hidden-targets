07a715d62e0f5c12cbfea09935280e7d8b61a48d
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-02-22 16:14:35 -0500

    The likelihood as a Gaussian function centered on the points on the loop

---
+++ place_field_map_for_likelihood.m
@@ -0,0 +1,37 @@
+function L1= place_field_map_for_likelihood(x_,y_,sigma,As,Omegas)
+%likelihood map initialization
+L1=zeros(size(Omegas,2),size(As,2));
+
+rgx=(As(end)-As(1));
+rgy=(Omegas(end)-Omegas(1));
+
+% express sigma in the normalized space: x pixels to distance in 
+% normalized A,O space
+sigma=sigma/numel(As);
+
+
+for key_pt=1:(numel(x_)) 
+   
+    % create X and Y matrices where the rows in X are the vector in As
+    % and the coloumn in Y are the vector in Omegas.
+    [X,Y]=meshgrid(As,Omegas);
+
+    % mux and muy are the action values
+    % of the ith key point on the loop
+    mux=x_(key_pt);
+    muy=y_(key_pt);
+
+    % temp place field like map from this keypoint   
+    temp=(1/(2*pi*(sigma^2))).* exp(-(...
+        ( ( (X-mux)./rgx ).^2 + ( (Y-muy)./rgy ).^2  )./(2*sigma^2) ));
+
+   
+    L1=L1+temp;
+
+  
+
+
+end
+
+
+end
