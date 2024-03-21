4314700c27b69b30cb171524c725add96f61a2d4
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-03-21 16:20:43 -0400

    Update plot_population_speed_heading_reward_rates.m

--- plot_population_speed_heading_reward_rates.m
+++ plot_population_speed_heading_reward_rates.m
@@ -5,8 +5,8 @@ figure;
 subplot(1,4,3), shadedErrorBar(1:trials, mean(corr_pop_avg(:,1:trials)) ,std( corr_pop_avg(:,1:trials))./sqrt(size(corr_pop_avg,1) )./scale,{'color','g'}); hold on;
 ylim([0 1]);
 hold on, subplot(1,4,1),shadedErrorBar(1:trials, mean(mu_pop_avg(:,1:trials)) ,std( mu_pop_avg(:,1:trials))./sqrt(size(mu_pop_avg,1) )./scale,{'color','g'}); hold on;
-
+ylim([1 1000])
 hold on, subplot(1,4,4), shadedErrorBar(1:trials, mean(anchors_no_pop(:,1:trials)) ,std( anchors_no_pop(:,1:trials))./sqrt(size(anchors_no_pop,1) )./scale,{'color','g'}); hold on;
-
+ylim([1 10])
 hold on, subplot(1,4,2), shadedErrorBar(1:trials, mean(hd_pop_avg(:,1:trials)) ,std( hd_pop_avg(:,1:trials))./sqrt(size(hd_pop_avg,1) )./scale,{'color','g'}); hold on
-    
+ylim([-pi/2 pi/2])    
