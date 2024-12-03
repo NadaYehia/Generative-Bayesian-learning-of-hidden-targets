1a2dda7465c6205c0b151ea548e2a96c81b359f3
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-12-03 17:48:56 -0500

    clnfiles

--- hidden/analyze_anchors_convergers_non_convergers.m
+++
@@ -1,2 +0,0 @@
-% plot the occupancy of the anchors in the action space, for the non
-% converging agents Vs the converging ones for the first 10 trials.
--- hidden/model_noise_process.m
+++
@@ -1,8 +0,0 @@
-% the noise distribution that best fits the noise dist. in the real mice
-% data 
-function noise_added=model_noise_process(mu_anchor)
-
-
-
-
-end
