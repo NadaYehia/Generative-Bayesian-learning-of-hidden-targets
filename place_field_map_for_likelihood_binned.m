4ec2256e08c915d4d257f2683040412a73207ba4
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2024-12-03 15:31:30 -0500

    Create place_field_map_for_likelihood_binned.m
    bin actions to the posterior discretization then smooth over it with gaussian masks

---
+++ place_field_map_for_likelihood_binned.m
@@ -0,0 +1,42 @@
+function L1= place_field_map_for_likelihood_binned(x_,y_,sigma,As,Omegas,wrkrs)
+
+L1=zeros(size(Omegas,2),size(As,2));
+rgx=(As(end)-As(1));
+rgy=(Omegas(end)-Omegas(1));
+
+% express sigma in the normalized space: x pixels to distance in 
+% normalized A,O space
+sigma=sigma/numel(As);
+[X,Y]=meshgrid(As,Omegas);
+
+for key_pt=1:numel(x_)
+    % create X and Y matrices where the rows in X are the vector in As
+    % and the coloumn in Y are the vector in Omegas.
+    
+    
+    % mux and muy are the action values
+    % of the ith key point on the loop
+    mux=x_(key_pt);
+    muy=y_(key_pt);
+    
+    % bin the mu_x and mu_y to the closest bins in As and Omegas
+
+    [~,idx_binX]=(min(abs(mux-As)));
+    [~,idx_binY]=(min(abs(muy-Omegas)));
+    
+    mux_b=As(idx_binX);
+    muy_b=Omegas(idx_binY);
+
+    % temp place field like map from this keypoint   
+    temp=(1/(2*pi*(sigma^2))).* exp(-(...
+        ( ( (X-mux_b)./rgx ).^2 + ( (Y-muy_b)./rgy ).^2  )./(2*(sigma^2)) ));
+    
+    L1=L1+temp;
+    temp=[];
+
+end
+
+
+
+  
+end
