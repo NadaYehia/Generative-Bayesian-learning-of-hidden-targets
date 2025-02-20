3bcf06961cb60264f7c3ddf8a5d621c53857b603
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2025-02-20 17:14:24 -0500

    delete debugging code

--- trajectory_planner_rerun_opti.m
+++
@@ -1,9 +0,0 @@
-% tto=tto+1;
-%      
-%     if(tto>MaxTrials) % if failed to find feasible solution
-%         % use input {r,theta anchors} in the next step.  
-%         optimal_para=[kd,r0,theta0];
-%         [~]=optimizer_obj.my_loss(optimal_para,arena,ka,numel(r0),w2,dt);
-% 
-%         break;
-%     end
