9d8e8370954a76409646da0dfeac1e23e77eb99d
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2025-02-21 11:36:26 -0500

    clean up and comment the code for setting up the posterior space

--- setup_posterior_space.m
+++ setup_posterior_space.m
@@ -1,18 +1,21 @@
 function [prior_flat,theta_bounds,r_bounds,r_after_home,arena_home_mask] = setup_posterior_space(Rs,Ths,env,radius_around_home)
 
-[prior_flat,theta_bounds,r_bounds]= set_control_actions_space(Rs,Ths,env.arena_dimensions);
-prior_flat=boolean(prior_flat);
+[prior_in_arena,theta_bounds,r_bounds]= set_control_actions_space(Rs,Ths,env.arena_dimensions);
 
-[prior_exc_home,c_home]=semi_circle_around_home(prior_flat,radius_around_home,Rs);
+% create binary mask with ones everywhere and 0s up till the distance
+% considered as Home.
+[~,c_home]=min(abs(Rs-radius_around_home));
+prior_minus_home=ones(size(prior));
+prior_minus_home(:,1:c_home)=0;
 r_after_home=Rs(c_home+1);
 
-prior_flat=prior_flat&prior_exc_home;
-arena_home_mask=prior_flat;
-naned_arena_home_mask=1-arena_home_mask;
-naned_arena_home_mask(find(naned_arena_home_mask))=nan;
-arena_home_mask=arena_home_mask+naned_arena_home_mask;
+% find regions of the posterior that are within the arena enclosure AND
+% outside home area
+prior_in_arena_minus_home=prior_in_arena & prior_minus_home;
+% add nans in the locations outside the arena bounds AND inside the home area 
+arena_home_mask=double(prior_in_arena_minus_home);
+arena_home_mask(arena_home_mask==0)=nan;
+%normalize the prior to sum to 1
 prior_flat=arena_home_mask./(nansum(arena_home_mask(:)));
-
-
 
 end
