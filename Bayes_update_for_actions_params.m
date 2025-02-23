95717be3672ac97af06781f0f86cac8e1073fa63
Nada Abdelrahman <43578602+NadaYehia@users.noreply.github.com>
2025-02-23 12:20:52 -0500

    update Baye's update code

--- Bayes_update_for_actions_params.m
+++ Bayes_update_for_actions_params.m
@@ -1,19 +1,29 @@
 function [posterior]=Bayes_update_for_actions_params...
     (L1,target_hit,prior)
+% Bayes_update_for_actions_params - Updates the prior belief using Bayes' 
+% rule based on whether the target was hit or not.
+% Inputs:
+%   L1          - Likelihood of the target being hit given the current parameters.
+%   target_hit  - Binary indicator (1 if target was hit, 0 otherwise).
+%   prior       - Prior belief distribution over the parameters.
+%
+% Output:
+%   posterior   - Updated belief distribution over the parameters after observing the target hit/miss.
 
 
 if (target_hit==1)
-        
-     posterior=Bayes_estimate(L1,1,prior);
-     posterior=posterior./( nansum(posterior(:)) );
      
-       
+    % If the target was hit, update the posterior using the likelihood L1.   
+     posterior=(L1).*(prior); % Multiply prior by likelihood L1.
+     posterior=posterior./( nansum(posterior(:)) ); % Normalize the posterior to sum to 1.
+        
          
 else
-         
+     % If the target was missed, update the posterior using the complement 
+     % of the likelihood (1 - L1).    
 
-         posterior=Bayes_estimate(L1,0,prior);
-         posterior=posterior./( nansum(posterior(:)) );
+     posterior=(1-L1).*prior; % Multiply prior by (1 - L1).
+     posterior=posterior./( nansum(posterior(:)) ); % Normalize the posterior to sum to 1.
         
           
 end
